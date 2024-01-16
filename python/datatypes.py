ee9e86532058a8f2e1c9fba33acc4ded7d388415
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-01-15 21:07:37 -0800

    added datatypes class and somecode examples

---
+++ python/datatypes.py
@@ -0,0 +1,12 @@
+#working with strings 
+name = "abdul hasib yousufzai"
+print(len(name))
+
+print(name[8])
+
+first_name = "abdul"
+last_name = "yousufzai"
+
+print("----------------------------------------------------------")
+fullname = first_name+" "+last_name
+print(fullname)
