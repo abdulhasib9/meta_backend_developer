4e792615118c69f6a3b03d614caee5420fe0c3d2
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-01-16 18:39:15 -0800

    added user_input class and code examples

---
+++ python/user_input.py
@@ -0,0 +1,10 @@
+# print('sep','tember',sep=",")
+
+num=input("Please enter a number: ")
+num1 = input("Please enter a second number: ")
+print(int(num) +int(num1))
+
+#using it as string concatenation 
+fname = input("please enter your first name: ")
+lname = input('please enter your last name: ')
+print("Hello {} {}".format(fname,lname))
