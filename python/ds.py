cac6b23d7c5614b784218bc85b206e160b0f3879
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-01-22 22:40:42 -0800

    added more code example and sorted the complex list with lambda expression

--- python/ds.py
+++ python/ds.py
@@ -54,3 +54,63 @@ for c in enumerate(new_list):
 for index,item in enumerate(new_list):
     print(index,item)
     
+print("##############################################################")
+#lambda demo 
+# list_tuples =[
+#     ("item1",20),
+#     ("item2",10),
+#     ("item3",4)
+# ]
+
+# list_tuples.sort(key=lambda items:list_tuples[1])
+# print(list_tuples)
+
+#adding new items 
+
+list2=list(range(10))
+list2.insert(5,200)
+print(list2)
+list2.append(400)
+print(list2)
+list2.pop()
+print(list2)
+list2.pop(5)
+print(list2)
+
+#removing unknown index item 
+list3= list("hello world")
+list3.remove("o")
+print(list3)
+for x in list3:
+    if x =="o":
+        list3.remove(x)
+    print(x)
+list3.clear()
+print(list3)
+
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+list4 = list(range(5))
+if 3 in list4:
+    print(list4.index(3))
+    
+print(list4.count(3))
+
+print("---------------------------------------------------------------")
+list5 =[23,44,1,34,56,3,444,32]
+#list5.sort(reverse=True)
+print(list5)
+print(sorted(list5, reverse=False))
+
+#sorting lists with complex objects
+list6=[
+    ("prod1",34),
+    ("prod2",4)
+
+]
+def item_sorted(item):
+    return[1]
+list6.sort(key=item_sorted,reverse=False)
+print(list6)
+
+list6.sort(key=lambda arguments : arguments[1])
+print(list6)
