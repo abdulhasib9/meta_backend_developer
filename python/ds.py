4cb8de282414cdbb1b2197f15d9de30808373546
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-01-22 21:53:13 -0800

    added ds class and code examples

---
+++ python/ds.py
@@ -0,0 +1,43 @@
+list2 =[1,"name",1.55]
+
+zeros = [0]*100
+print(*zeros)
+
+list_of_lists = [[12,34],[23,45]]
+
+print(list_of_lists)
+
+combined = list2+zeros
+print(combined)
+
+print("=====================================================================")
+#generating numbers 0-19 in a list using list() funcution 
+numbers = list(range(20))
+print(numbers)
+
+print("----------------------------------------------------------------")
+#we can pass string object in list function because string is iterable 
+chars = list("Hello World!")
+print(chars)
+
+print(len(zeros))
+
+print("------------------------------------------------------------------")
+
+#unpacking list
+nums = [1,2,4]
+num1,num2,num3 = nums
+
+print(num1)
+print(num2)
+print(num3)
+
+print("----------------------------------------------------------")
+
+#unpacking specific items 
+specific = list(range(20))
+print(specific)
+one,two, *others=specific
+print(one)
+print(two)
+print(others)
