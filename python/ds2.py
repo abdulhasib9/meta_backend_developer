e7d7ef8248dff9e6ff83836e367f735c6bce9449
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-01-22 22:49:39 -0800

    added ds2 class and code examples for map function

---
+++ python/ds2.py
@@ -0,0 +1,19 @@
+items =[
+    ("item1",23),
+    ("item2",4),
+    ("item3",55)
+]
+
+prices = []
+
+for x in items:
+    prices.append(x[1])
+print(prices)
+
+s = map(lambda item:item[1],items )
+print(s)
+for x in s:
+    print(x)
+    
+ss=list(map(lambda item:item[1],items))
+print(ss)
