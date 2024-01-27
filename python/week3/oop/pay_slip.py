b29eeb6fa6c5e17610466e03687f0672588d0dc3
abdul hasib yousufzai <youtusufzai.abdulhasib@gmail.com>
2024-01-27 14:03:05 -0800

    refactored class payslip.py

--- python/week3/oop/pay_slip.py
+++ python/week3/oop/pay_slip.py
@@ -1,20 +1,21 @@
 class pay:
-    def __init__(self,input_name,payment,amount) -> None:
-        self.name= input_name
-        self.payment= payment
-        self.amount = amount 
+    def __init__(self, input_name, payment, amount) -> None:
+        self.name = input_name
+        self.payment = payment
+        self.amount = amount
 
     def pay_now(self):
         self.payment = True
 
     def pay_status(self):
-        if self.payment == True:
+        if self.payment:
             print(self.name + " got payed " + str(self.amount))
         else:
-            print(self.name +" has not been payed yet" )
+            print(self.name + " has not been payed yet")
 
-hasib = pay("hasib",False,30000)
-noman = pay("Noman","no",4000)
+
+hasib = pay("hasib", False, 30000)
+noman = pay("Noman", "no", 4000)
 
 hasib.pay_status()
 noman.pay_status()
