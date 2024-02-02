eb1da6c9d428d178eaef312622bb0de17661fe07
abdul hasib yousufzai <youtusufzai.abdulhasib@gmail.com>
2024-02-01 23:24:01 -0800

    added more code examples to app class

--- python/pcep-prep/app.py
+++ python/pcep-prep/app.py
@@ -1,4 +1,73 @@
-multi_line = """
-this is python variable with multiple lines 
-"""
-print(multi_line)
+# multi_line = """
+# this is python variable with multiple lines 
+# """
+# print(multi_line)
+
+# print(list(multi_line))
+# x,y=1,3
+
+# print(y,x)
+
+
+# age: int =20    
+# print( id (age))
+# age = "name"
+# print ( id(age))
+# print(age)
+
+# x=[1,2,4]
+# print(id(x))
+
+# x.append(5)
+# print(id(x))
+
+# string= "python programming"
+# print(string[-2])
+
+# print(string[0:3])
+
+# print(id(string))
+
+# string += " another"
+
+# print(string)
+
+# print(id(string))
+
+# print(id(string[1]))
+
+# first = "abdul hasib"
+# last = "yousufzai"
+
+# full_name = f"{first} {last}"
+
+# print(full_name)
+
+# print(full_name.upper())
+# print(full_name.lower())
+# print(full_name.rstrip())
+# print(full_name.title())
+# print(full_name.find("h"))
+# print(full_name.replace("a","_"))
+
+# print("hasib" in full_name)
+# print("hasib" not in full_name)
+
+
+# x = 0b101
+
+# print(x)
+
+# print(hex(x))
+
+# x=10+3
+# x=10-3
+# x=10//3
+# print(x)
+# x=10/3
+# print(type(x),x)
+# print(10**3)
+
+x = input("x: ")
+
+print(bool(x))
