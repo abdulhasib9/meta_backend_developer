740dec67fb5c7f9b694fb47edc5a500051af2488
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-02-11 23:15:26 -0800

    adde changes to gitignore file

---
+++ python/pcep-prep/builtinlibrary/zipfunction.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+# with ZipFile("test.zip","w") as zip:
+#     for path in Path("builtinlibrary/").rglob("*.*"):
+#         zip.write(path)
+
+# with ZipFile("test.zip") as zip:
+#     print(zip.namelist())
