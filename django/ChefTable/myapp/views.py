b8726ecd658185d556980883a3683cc4c7adfefb
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-02-25 22:15:33 -0800

    added changes to cheftable project and added brand new carapp

--- django/ChefTable/myapp/views.py
+++ django/ChefTable/myapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World from my app")
+def home(request):
+    content ="<html><body><h1>Home Page </h1></body></html>"
+    return HttpResponse(content)
+    
